6cded850d1960f9825a6d0f436a0402c803c6052
Rocc  Colonna <colonna.rocco2000@gmail.com>
2021-05-28 17:44:25 +0200

    parte iniziale

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+class User {
+    public $name;
+
+    public $lastname;
+
+    public $username;
+
+    public $email;
+
+    public $password;
+
+}
+
+// Istanza Marco
+$marco = new User ();
+
+// Istanza Simone
+$simone = new User();
+
+// Istanza Anna
+$anna = new User();
+
+var_dump($marco)
+var_dump($simone)
+var_dump($anna)
+
+?>
